94e9d2729dfe51fef8d88250763918f6135c0942
Nauman Ahmed <naumanahmedraja@gmail.com>
2023-05-08 09:51:36 +0200

    A singularity and docker container with 25 tools is now working with a CWL example.

--- cwl/example1/run_comet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-conda run --no-capture-output -n comet-ms comet -p
-ls /data
-cp /data/comet.params.new /cwl/example1/comet.params
-#cwl-runner /cwl/example1/workflow.cwl --input2 /cwl/example1/up00000062.fasta --input1 /cwl/example1/2021-10-8_Ecoli.mzML
