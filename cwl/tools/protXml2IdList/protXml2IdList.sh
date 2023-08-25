185ccf1f27f198d7b84113b5b0788c56993ee850
Nauman Ahmed <naumanahmedraja@gmail.com>
2023-08-26 00:41:24 +0200

    Update protXml2IdList/protXml2IdList.cwl

--- cwl/tools/protXml2IdList/protXml2IdList.sh
+++ cwl/tools/protXml2IdList/protXml2IdList.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-conda run -n protxml2idlist xmllint --xpath "//*[local-name()='protein']/@protein_name" $1 | cut -d '|' -f2
+conda run -n protxml2idlist xmllint --xpath "//*[local-name()='protein']/@protein_name" $1 | cut -d '|' -f2 > idlist.txt
