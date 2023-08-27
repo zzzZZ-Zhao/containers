dba1ef35cb26bd790f842c1cd1b08001684ab31d
Nauman Ahmed <naumanahmedraja@gmail.com>
2023-08-28 01:09:07 +0200

    Updated gProfiler.cwl

--- cwl/tools/gProfiler/gProfiler.sh
+++ cwl/tools/gProfiler/gProfiler.sh
@@ -3,4 +3,6 @@ set -e
 
 
 # Construct the curl command
-curl -X 'POST' -d '{"organism": "hsapiens", "query": ['"$(cat "$1" | tr '\n' ',' | sed 's/,$//' | sed 's/,/","/g')"]}\' 'https://biit.cs.ut.ee/gprofiler/api/gost/profile/' > output.txt
+#curl -X 'POST' -d '{"organism": "hsapiens", "query": ['"$(cat "$1" | tr '\n' ',' | sed 's/,$//' | sed 's/,/","/g')"]}\' 'https://biit.cs.ut.ee/gprofiler/api/gost/profile/' > output.txt
+awk ‘BEGIN {command="curl -X 'POST' -d '{\"organism\": \"hsapiens\", \"query\": ["}  (NR==1){command = command "\"" $1 "\""} (NR>1){command = command ", \"" $1 "\""} END {command = command "]}' 'https://biit.cs.ut.ee/gprofiler/api/gost/profile/' > output.txt" ; system(command)} $1
+
