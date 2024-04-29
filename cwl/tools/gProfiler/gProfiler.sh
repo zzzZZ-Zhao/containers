fa903b23f75a9ab7a11969dadf488e668acf615e
Vedran Kasalica <v.kasalica@esciencecenter.nl>
2024-04-30 00:00:50 +0200

    Fix gProfiler.sh

--- cwl/tools/gProfiler/gProfiler.sh
+++ cwl/tools/gProfiler/gProfiler.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
-set -e
 
+# Check if a file was provided as an argument
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <filename>"
+  exit 1
+fi
 
-# Construct the curl command
-awk ‘BEGIN {command="curl -X 'POST' -d '{\"organism\": \"hsapiens\", \"query\": ["}  (NR==1){command = command "\"" $1 "\""} (NR>1){command = command ", \"" $1 "\""} END {command = command "]}' 'https://biit.cs.ut.ee/gprofiler/api/gost/profile/' > output.json" ; system(command)} $1
+# Assign the file name to a variable
+filename=$1
 
+# Check if the file exists
+if [ ! -f "$filename" ]; then
+  echo "File not found: $filename"
+  exit 1
+fi
+
+# Initialize the command
+command="curl -X 'POST' -d '{\"organism\": \"hsapiens\", \"query\": ["
+
+# Read from the file and process lines
+first=1
+while read line
+do
+  if [ "$first" -eq 1 ]; then
+    command="${command}\"$line\""
+    first=0
+  else
+    command="${command}, \"$line\""
+  fi
+done < "$filename"
+
+# Finish assembling the command
+command="${command}], \"significance_threshold_method\": \"false_discovery_rate\"}' 'https://biit.cs.ut.ee/gprofiler/api/gost/profile/' > output.json"
+
+# Execute the command
+eval $command
